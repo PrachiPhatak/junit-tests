47b7a73085d8beae3de00dd35341491bdf6c64ad
PrachiPhatak <phatak.prachi@gmail.com>
2021-07-22 17:06:21 -0500

    Adds update and delete and tests

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Student {
 
     private long id;
     private String name;
-    private ArrayList<Integer> grades ;
+    private ArrayList<Integer> grades;
 
     public Student(long id, String name) {
         this.id = id;
@@ -13,28 +14,44 @@ public class Student {
     }
 
     // returns the student's id
-    public long getId(){ return id;}
+    public long getId() {
+        return id;
+    }
 
     // returns the student's name
-    public String getName(){ return name;}
+    public String getName() {
+        return name;
+    }
 
     // adds the given grade to the grades list
-    public void addGrade(int grade){
+    public void addGrade(int grade) {
         grades.add(grade);
     }
 
     // returns the list of grades
-    public ArrayList<Integer> getGrades(){
+    public ArrayList<Integer> getGrades() {
         return grades;
     }
 
     // returns the average of the students grades
-    public double getGradeAverage(){
+    public double getGradeAverage() {
         double total = 0;
         for (Integer grade : grades) {
             total = total + grade;
         }
-        return total/grades.size();
+        return total / grades.size();
+    }
+
+    public void updateGrade(int newGrade) {
+        Collections.sort(grades);
+        if (grades.size() > 0)
+            grades.set(0, newGrade);
+    }
+
+    public void deleteGrade() {
+        Collections.sort(grades);
+        if (grades.size() > 0)
+            grades.remove(0);
     }
 
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -10,23 +10,23 @@ public class StudentTest {
     Student student;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         student = new Student(12, "Prachi");
         student.addGrade(90);
     }
 
     @Test
-    public void testGetIDReturnsLong(){
+    public void testGetIDReturnsLong() {
         assertEquals(12, student.getId());
     }
 
     @Test
-    public void testGetName(){
+    public void testGetName() {
         assertEquals("Prachi", student.getName());
     }
 
     @Test
-    public void testAddGrade(){
+    public void testAddGrade() {
         assertEquals(1, student.getGrades().size());
 
         student.addGrade(100);
@@ -35,20 +35,93 @@ public class StudentTest {
         assertEquals(4, student.getGrades().size());
     }
 
-   @Test
-    public  void testGetGradeAvg(){
-       student.addGrade(90);
-       student.addGrade(80);
-       student.addGrade(100);
+    @Test
+    public void testGetGradeAvg() {
+        student.addGrade(90);
+        student.addGrade(80);
+        student.addGrade(100);
 
-       assertEquals(90.0, student.getGradeAverage());
-   }
+        assertEquals(90.0, student.getGradeAverage());
+    }
 
-   @Test
-    public void testGetGrades(){
+    @Test
+    public void testGetGrades() {
         ArrayList<Integer> grades = new ArrayList<>();
         grades.add(90);
-       assertEquals(grades, student.getGrades());
-   }
+        assertEquals(grades, student.getGrades());
+    }
+
+    @Test
+    public void testDeleteGrades_arrayOf3_returnArrayOf2() {
+        student = new Student(12, "Prachi");
+        student.addGrade(90);
+        student.addGrade(80);
+        student.addGrade(100);
+        ArrayList<Integer> expectedGrades = new ArrayList<>();
+        expectedGrades.add(90);
+        expectedGrades.add(100);
+
+        student.deleteGrade();
+        assertEquals(2, student.getGrades().size());
+        assertEquals(expectedGrades, student.getGrades());
+    }
+
+    @Test
+    public void testDeleteGrades_null_return0() {
+        student = new Student(12, "Prachi");
+
+        student.deleteGrade();
+        assertEquals(0, student.getGrades().size());
+    }
+    @Test
+    public void testDeleteGrades_arrayOfOneGrade_return0() {
+        student = new Student(12, "Prachi");
+        student.addGrade(90);
+
+        student.deleteGrade();
+        assertEquals(0, student.getGrades().size());
+    }
+
+    @Test
+    public void testDeleteGrades_arrayOf3SameGrades_returnArrayOf2() {
+        student = new Student(12, "Prachi");
+        student.addGrade(90);
+        student.addGrade(90);
+        student.addGrade(90);
+
+        student.deleteGrade();
+        assertEquals(2, student.getGrades().size());
+    }
+
+
+
+    @Test
+    public void testUpdateGrades_arrayOfOneGrade_return0() {
+        student = new Student(12, "Prachi");
+        student.addGrade(90);
+        ArrayList<Integer> grades = new ArrayList<>();
+        grades.add(99);
+
+        student.updateGrade(99);
+
+        assertEquals(1, student.getGrades().size());
+        assertEquals(grades, student.getGrades());
+    }
+
+    @Test
+    public void testUpdateGrades_arrayOfTwoSameGrade_return0() {
+        student = new Student(12, "Prachi");
+        student.addGrade(90);
+        student.addGrade(90);
+        ArrayList<Integer> grades = new ArrayList<>();
+        grades.add(99);
+        grades.add(90);
+
+        student.updateGrade(99);
+
+        assertEquals(2, student.getGrades().size());
+        assertEquals(grades, student.getGrades());
+    }
+
 
 }
